297cf394575446df0225fee304002bd74ef08b63
Moussa Kouyate <mobytech91@gmail.com>
2026-09-05 14:25:26 +0200

    fix: mise a jour de API_URL vers le nouveau backend Next.js Vercel

--- gym-frontend/src/config.ts
+++ gym-frontend/src/config.ts
@@ -1,4 +1,3 @@
 import { Platform } from 'react-native';
 
-// export const API_URL: string = 'http://192.168.1.10:8080/api';
-export const API_URL: string = 'https://gym-pro-fs2k.onrender.com/api';
+export const API_URL: string = 'https://gym-bd.vercel.app/api';
